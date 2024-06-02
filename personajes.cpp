0626e8f7f9b16f10a3af5ef9d2bc55c60bbb057c
jacvelez <164963781+jacvelez@users.noreply.github.com>
2024-06-01 20:28:18 -0500

    Update personajes.cpp
    se le agrega un nuevo metodo para poder mover personajes que no controla el jugador, ademas de sobrecargar el constructor de personajes para la creacion del mismo

--- personajes.cpp
+++ personajes.cpp
@@ -12,6 +12,21 @@ Personaje::Personaje(const QString &rutaSprite, int totalFrames, int fila, QGrap
     setPixmap(frameInicial);
 }
 
+Personaje::Personaje(const QString &rutaSprite, int totalFrames, int fila, float x, float y, QGraphicsItem *parent)
+    : QGraphicsPixmapItem(parent), frameActual(0), totalFrames(totalFrames), fila(fila),
+    modoEspecial(false), animando(false)
+{
+    hojaSprite.load(rutaSprite);
+    anchoFrame = hojaSprite.width() / totalFrames;
+    altoFrame = hojaSprite.height() / 4; // Suponiendo que hay 5 filas en total
+
+    QPixmap frameInicial = hojaSprite.copy(frameActual * anchoFrame, fila * altoFrame, anchoFrame, altoFrame);
+    setPixmap(frameInicial);
+    setPos(x, y); // Establecer la posición inicial del NPC
+}
+
+
+Personaje::~Personaje() {}
 void Personaje::siguienteFrame(bool modoEspecial) {
     frameActual = (frameActual + 1) % totalFrames;
     int yOffset = modoEspecial ? 0 : fila * altoFrame;
@@ -42,4 +57,17 @@ void Personaje::alternarModoEspecial() {
 
 bool Personaje::estaAnimando() const {
     return animando;
+}
+
+
+void Personaje::moverPersonajeAdicional(int velocidad, float minX, float maxX) {
+    // Mover el NPC hacia adelante con la velocidad especificada
+    float nuevaPosX = x() + velocidad;
+    // Verificar si el NPC ha alcanzado los límites de movimiento horizontal
+    if (nuevaPosX < minX) {
+        nuevaPosX = maxX; // Si el NPC se sale del límite izquierdo, volver al límite derecho
+    } else if (nuevaPosX > maxX) {
+        nuevaPosX = minX; // Si el NPC se sale del límite derecho, volver al límite izquierdo
+    }
+    setX(nuevaPosX);
 }
